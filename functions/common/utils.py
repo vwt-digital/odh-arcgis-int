3fe91df2ed98f6600fd91cb6bbf3065c21c0febc
Rick van Sloten <r.vansloten@vwt.digital>
2022-02-21 14:41:21 +0100

    ODH-26; Added documentation ✓ changes have been validated

--- functions/common/utils.py
+++ functions/common/utils.py
@@ -3,12 +3,12 @@ from dataclasses import dataclass
 
 from google.cloud.secretmanager import (
     SecretManagerServiceClient as Client,
-    GetSecretVersionRequest,
-    AccessSecretVersionRequest,
-    AccessSecretVersionResponse,
-    AddSecretVersionRequest,
-    SecretPayload,
-    SecretVersion,
+    GetSecretVersionRequest as GetRequest,
+    AccessSecretVersionRequest as AccessRequest,
+    AccessSecretVersionResponse as AccessResponse,
+    AddSecretVersionRequest as AddRequest,
+    SecretPayload as Payload,
+    SecretVersion as Version,
     ReplicationStatus
 )
 
@@ -18,10 +18,19 @@ class Secret:
     value: bytes
     create_time: datetime
     destroy_time: datetime
-    state: SecretVersion.State
+    state: Version.State
     replication_status: ReplicationStatus
 
-    def get_value(self, encoding: str = "UTF-8"):
+    def get_value(self, encoding: str = "UTF-8") -> str:
+        """
+        Returns the secret value.
+
+        :param encoding: Encoding to use to convert bytes to string.
+        :type encoding: str
+
+        :return: The secret value.
+        :rtype: str
+        """
         return self.value.decode(encoding)
 
 
@@ -29,24 +38,24 @@ def get_secret(project_id: str, secret_id: str, version: str = "latest") -> Secr
     """
     Returns a Secret Manager secret.
 
-    :param project_id: Google Cloud project ID
+    :param project_id: Google Cloud project ID.
     :type project_id: str
-    :param secret_id: Google Cloud Secret Manager secret ID
+    :param secret_id: Google Cloud Secret Manager secret ID.
     :type project_id: str
-    :param version: Version of the secret
+    :param version: Version of the secret.
     :type version: str
 
-    :return: Secret value
-    :rtype: str
+    :return: The secret.
+    :rtype: Secret
     """
     client = Client()
     path = client.secret_version_path(project_id, secret_id, version)
 
-    secret_version_request = GetSecretVersionRequest(path)
-    secret_version_access_request = AccessSecretVersionRequest(path)
+    secret_version_request = GetRequest(path)
+    secret_version_access_request = AccessRequest(path)
 
-    secret_version: SecretVersion = client.get_secret_version(request=secret_version_request)
-    secret_access: AccessSecretVersionResponse = client.access_secret_version(request=secret_version_access_request)
+    secret_version: Version = client.get_secret_version(request=secret_version_request)
+    secret_access: AccessResponse = client.access_secret_version(request=secret_version_access_request)
 
     return Secret(
         value=secret_access.payload.data,
@@ -58,10 +67,20 @@ def get_secret(project_id: str, secret_id: str, version: str = "latest") -> Secr
 
 
 def update_secret(project_id: str, secret_id: str, value: bytes) -> None:
+    """
+    Updates a secret to a new version.
+
+    :param project_id: Google Cloud project ID.
+    :type project_id: str
+    :param secret_id: Google Cloud Secret Manager secret ID.
+    :type project_id: str
+    :param value: Secret value as bytes.
+    :type value: bytes
+    """
     client = Client()
     path = client.secret_path(project_id, secret_id)
 
-    secret_payload = SecretPayload(value)
-    secret_version_add_request = AddSecretVersionRequest(path, payload=secret_payload)
+    secret_payload = Payload(value)
+    secret_version_add_request = AddRequest(path, payload=secret_payload)
 
     client.add_secret_version(request=secret_version_add_request)
